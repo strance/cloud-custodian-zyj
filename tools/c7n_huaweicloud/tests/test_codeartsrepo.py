0cd18b1b437a91b5f25342c999537cf69a0dd53e
FDH666 <FDH666@noreply.gitcode.com>
2025-08-23 13:50:35 +0800

    添加开启水印测试用例

--- tools/c7n_huaweicloud/tests/test_codeartsrepo.py
+++ tools/c7n_huaweicloud/tests/test_codeartsrepo.py
@@ -11,3 +11,23 @@ class CodeArtsRepoTest(BaseTest):
         )
         resources = p.run()
         self.assertEqual(len(resources), 1)
+
+    def test_open_watermark(self):
+        factory = self.replay_flight_data("codeartsrepo_open_watermark")
+        p = self.load_policy(
+            {
+                "name": "codeartsrepo_open_watermark",
+                "resource": "huaweicloud.codeartsrepo-project",
+                "filters": [
+                    {
+                        "type": "value",
+                        "key": "id",
+                        "value": "f3754a7faff44f7e92e10caa4cf7ca0c",
+                    }
+                ],
+                "actions": [{"type": "open"}],
+            },
+            session_factory=factory,
+        )
+        resources = p.run()
+        self.assertEqual(resources[0]["project_id"], "f3754a7faff44f7e92e10caa4cf7ca0c")
